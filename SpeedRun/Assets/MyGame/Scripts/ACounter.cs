cae02130abb96508596b782612de610044b56058
leoeichinger <eichinger.l@gmx.at>
2021-04-13 09:30:54 +0200

    added beginning of socre counter

---
+++ SpeedRun/Assets/MyGame/Scripts/ACounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ACounter : MonoBehaviour
+{   
+    public AddScore adscore;
+    public int acount = 0;
+    public Text answer;
+
+    void Start() {
+        answer.text = adscore.counter;
+    }
+}
---
+++ SpeedRun/Assets/MyGame/Scripts/AddScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+   public ACounter ac;
+
+   int add = 1;
+
+   public int counter;
+
+   public int acount = 0;
+
+   public void AddCount()
+   {
+       counter = ac.acount + add;
+   }
+}
