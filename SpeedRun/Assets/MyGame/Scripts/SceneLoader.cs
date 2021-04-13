7fc62f5dc28380e449179cb111fed98968ef164a
leoeichinger <eichinger.l@gmx.at>
2021-04-13 09:08:49 +0200

    inital commit

---
+++ SpeedRun/Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+}
